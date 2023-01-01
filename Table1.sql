a0d7b58c253e5d5d8a5e7bc4017c6c813c00810d
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2023-01-01 22:04:36 +0100

    Init commit

---
+++ Table1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [firstname] NCHAR(100) NOT NULL, 
+    [lastname] NCHAR(100) NOT NULL, 
+    [adress] NCHAR(100) NOT NULL, 
+    [city] NCHAR(10) NOT NULL, 
+    [mobile] NCHAR(10) NULL
+)
+
+
